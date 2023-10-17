3878667dd38bb6cb4028050f5c3df0b48c0e459e
Sypacije <146341322+Sypacije@users.noreply.github.com>
2023-10-17 09:45:35 +0400

    Add files via upload

---
+++ Задание 2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int k;
+    float p, a;
+    cout << "Введите периметр и коэффициент K: ";
+    cin >> p >> k;
+
+    if (k < 0) {
+        cout << "Неверное значение K." << endl;
+        system("pause");
+        return 1;
+    }
+
+    else {
+        a = (p - k) / 3;
+
+        if ((2 * a <= (a + k)) or ((2 * a + k) <= a)) {
+            cout << "Треугольник не существует." << endl;
+            system("pause");
+            return 1;
+        }
+
+        else {
+            cout << "Длина боковой стороны треугольника равнa " << a << endl;
+            system("pause");
+            return 0;
+        }
+    }
+
+}
+
